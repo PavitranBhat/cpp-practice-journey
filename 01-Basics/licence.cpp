cbe6f6a1cb84880b2227b9986968b45238cdca6d
Pavitra Bhat <161752419+PavitranBhat@users.noreply.github.com>
2025-08-21 18:58:53 +0530

    Add files via upload

---
+++ 01-Basics/licence.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter the age:";
+    cin>> age;
+    if(age>=18)
+    cout<<"u will get licence";
+    else
+    cout<<"u will not get licence";
+    return 0;
+}
