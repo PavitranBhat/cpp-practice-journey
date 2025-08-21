556943e268d4ec15a587d9219a0daad0a56f0011
Pavitra Bhat <161752419+PavitranBhat@users.noreply.github.com>
2025-08-21 18:58:28 +0530

    Add files via upload

---
+++ 01-Basics/Max.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "enter the value of a:";
+    cin >> a;
+    cout <<"enter the value for b:";
+    cin >> b;
+    if(a>b)
+        cout<<" a is greater";
+    else
+        cout << "b is greater";
+    return 0;
+
+}
